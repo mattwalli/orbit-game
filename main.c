0ef4519e4196e91f9bd4fa29d800c78e7f57b1e2
mattwalli <mattwalli@gmail.com>
2020-04-18 16:20:52 -0700

    SDL2 image library

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 #include <stdbool.h>
 #include <math.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 800;
 
 int main(void)
 {
     SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Surface* image = NULL;
+    SDL_Texture* texture = NULL;
 
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
@@ -26,37 +29,58 @@ int main(void)
         }
         else
         {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+            //Initialize PNG loading
+            if( !( IMG_Init( IMG_INIT_PNG ) & IMG_INIT_PNG ) )
+            {
+                printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+            }
 
-            //Main loop flag
-            bool quit = false;
+            //Load textures
+            renderer = SDL_CreateRenderer(window, -1, 0);
+            image = IMG_Load("earth.png");
+            if( image == NULL )
+            {
+                printf( "Unable to load image! SDL_image Error: %s\n", IMG_GetError() );
+            }
+            texture = SDL_CreateTextureFromSurface(renderer, image);
+            if( texture == NULL )
+            {
+                printf( "Unable to load texture! SDL_image Error: %s\n", IMG_GetError() );
+            }
 
             //Event handler
-            SDL_Event e;
+            SDL_Event event;
 
-            //While application is running
+            //Main loop
+            bool quit = false;
             while( !quit )
             {
                 //Handle events on queue
-                while( SDL_PollEvent( &e ) != 0 )
+                while( SDL_PollEvent( &event ) != 0 )
                 {
-                    //User requests quit
-                    if( e.type == SDL_QUIT )
+                    switch(event.type)
                     {
+                    case SDL_QUIT: //User requested quit
                         quit = true;
+                        break;
                     }
                 }
+
+                //Decorate the surface
+                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderPresent(renderer);
             
                 //Update the surface
                 SDL_UpdateWindowSurface( window );
             }
         }
     }
-    //Destroy window
+    //Cleanup
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow( window );
-
-    //Quit SDL subsystems
+    IMG_Quit();
     SDL_Quit();
 
     return 0;
