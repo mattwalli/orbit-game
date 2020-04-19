52f732ac391c3000cf7c296bbc8277b644b8d37b
mattwalli <mattwalli@gmail.com>
2020-04-18 17:17:22 -0700

    iterated sprite loading

--- main.c
+++ main.c
@@ -4,15 +4,29 @@
 #include <math.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 800;
+
+// Main function handles creation of window, loading sprites, event capture, and rendering.
+// I am new to SDL2. This code is hobbled together from multiple tutorials.
+// https://lazyfoo.net/tutorials/SDL/ and http://www.programmersranch.com/p/sdl2-tutorials.html
 
 int main(void)
 {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 800;
+    const int NUM_SPRITES = 4;
+    const char* const SPRITE_FILES[] = {
+        "fire.png",
+        "rocket.png",
+        "satellite.png",
+        "earth.png"
+    };
+
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
     SDL_Surface* image = NULL;
     SDL_Texture* texture = NULL;
+
+    SDL_Texture* sprites[NUM_SPRITES] = { NULL };
 
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
@@ -35,17 +49,24 @@ int main(void)
                 printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
             }
 
-            //Load textures
-            renderer = SDL_CreateRenderer(window, -1, 0);
-            image = IMG_Load("earth.png");
-            if( image == NULL )
+            //Load sprites
+            renderer = SDL_CreateRenderer( window, -1, 0 );
+            for( int i = 0; i < NUM_SPRITES; i++ )
             {
-                printf( "Unable to load image! SDL_image Error: %s\n", IMG_GetError() );
-            }
-            texture = SDL_CreateTextureFromSurface(renderer, image);
-            if( texture == NULL )
-            {
-                printf( "Unable to load texture! SDL_image Error: %s\n", IMG_GetError() );
+                image = IMG_Load( SPRITE_FILES[i] );
+                if( image == NULL )
+                {
+                    printf( "Unable to load image! SDL_image Error: %s\n", IMG_GetError() );
+                }
+                else
+                {
+                    sprites[i] = SDL_CreateTextureFromSurface( renderer, image );
+                    if( sprites[i] == NULL )
+                    {
+                        printf( "Unable to load texture! SDL_image Error: %s\n", IMG_GetError() );
+                    }
+                    SDL_FreeSurface( image );
+                }
             }
 
             //Event handler
@@ -58,7 +79,7 @@ int main(void)
                 //Handle events on queue
                 while( SDL_PollEvent( &event ) != 0 )
                 {
-                    switch(event.type)
+                    switch( event.type )
                     {
                     case SDL_QUIT: //User requested quit
                         quit = true;
@@ -67,7 +88,7 @@ int main(void)
                 }
 
                 //Decorate the surface
-                SDL_RenderCopy(renderer, texture, NULL, NULL);
+                SDL_RenderCopy(renderer, sprites[3], NULL, NULL);
                 SDL_RenderPresent(renderer);
             
                 //Update the surface
@@ -76,9 +97,8 @@ int main(void)
         }
     }
     //Cleanup
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(image);
-    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture( texture );
+    SDL_DestroyRenderer( renderer );
     SDL_DestroyWindow( window );
     IMG_Quit();
     SDL_Quit();
